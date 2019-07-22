7c4f9a5d7c883031bdf3117a33d75501ca6677e1
dirkliu <liuqi0702@126.com>
2019-07-23 00:27:45 +0800

    gulp test1

--- gulp-learn/gulpfile.js
+++ gulp-learn/gulpfile.js
@@ -1,65 +1,8 @@
 var gulp = require('gulp');
+var babel = require('gulp-babel');
 
-var uglify = require('gulp-uglify');
-var concat = require('gulp-concat');
-var autoprefix = require('gulp-autoprefixer');
-var gulpif = require('gulp-if');
-var sass = require('gulp-sass');
-var del = require('del');
-var st = require('st');
-var connect=require('gulp-connect');
-var open = require('gulp-open');
-var proxy = require('http-proxy-middleware');
-
-gulp.task('clean', function () {
-    return del(['dist']);
+ gulp.task("default", function () {
+  return gulp.src("src/app.js")
+    .pipe(babel())
+    .pipe(gulp.dest("dist"));
 });
-
-gulp.task('sass', function () {
-    gulp.src('src/styles/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('dist/styles'))
-        .pipe(connect.reload());
-});
-
-gulp.task('html', function () {
-    return gulp.src('src/index.html')
-        .pipe(gulp.dest('dist'))
-        .pipe(connect.reload());
-});
-
-gulp.task('json', function () {
-  return gulp.src('src/test.json')
-    .pipe(gulp.dest('dist'))
-    .pipe(connect.reload())
-});
-
-gulp.task('server', function (done) {
-    connect.server({
-        root:'dist',
-        livereload: true,
-        middleware: function (connect, opt) {
-          return [
-            proxy('/**/*.action', {
-              target: 'http://dangerous.com',
-              changeOrigin:true
-            })
-          ]
-        }
-    });
-});
-
-gulp.task('watch', function () {
-    gulp.watch('src/styles/*.scss', ['sass']);
-    gulp.watch('src/*.html', ['html']);
-});
-
-gulp.task('open', function () {
-    gulp.src('src/')
-        .pipe(open({
-            uri:'http://localhost:8080',
-            app:'chrome'
-        }))
-});
-
-gulp.task('serve', ['clean', 'sass', 'json', 'html', 'server','open','watch']);
---
+++ gulp-learn/src/app.js
@@ -0,0 +1,5 @@
+import Home from './home/Home'
+
+var home = new Home()
+
+console.log('home:', home.name)
