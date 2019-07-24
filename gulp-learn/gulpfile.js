42a2eebd1aa2b1fabd83033744310242629be964
dirkliu <liuqi0702@126.com>
2019-07-25 00:07:07 +0800

    add sass

--- gulp-learn/gulpfile.js
+++ gulp-learn/gulpfile.js
@@ -1,7 +1,10 @@
 var gulp = require('gulp')
 var uglify = require('gulp-uglify')
 var concat = require('gulp-concat')
+var sass = require('gulp-sass')
 var babel = require('gulp-babel')
+
+sass.compiler = require('node-sass')
 
  gulp.task("default", function () {
   return gulp.src("src/app.js")
@@ -15,6 +18,12 @@ var babel = require('gulp-babel')
  		.pipe(uglify())
  		.pipe(gulp.dest("dist/home"));
  })
+
+ gulp.task('sass', function () {
+  return gulp.src('src/styles/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('dist/styles'));
+});
 
  gulp.task("public", function () {
  	return gulp.src("src/public/**/*.js")
